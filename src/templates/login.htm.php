cc787dd3a583c38aba984072bc9e62648a575e76
frithjofhoppe <frithjof.hoppe@icloud.com>
2018-03-16 21:23:25 +0100

    modified registration

--- src/templates/login.htm.php
+++ src/templates/login.htm.php
@@ -1,24 +1,3 @@
-<!--<div class="col-md-12">-->
-<!--<form name="kontakt" class="form-horizontal form-condensed" action="" method="post">-->
-<!--  <div class="form-group control-group">-->
-<!--	<label class="control-label col-md-offset-2 col-md-2" for="email">E-Mail</label>-->
-<!--	<div class="col-md-4">-->
-<!--	  <input type="email" class="form-control" id="email" name="email" value="" />-->
-<!--	</div>-->
-<!--  </div>-->
-<!--  <div class="form-group control-group">-->
-<!--	<label class="control-label col-md-offset-2 col-md-2" for="password">Password</label>-->
-<!--	<div class="col-md-4">-->
-<!--	  <input type="password" class="form-control" id="password" name="password" value="" />-->
-<!--	</div>-->
-<!--  </div>-->
-<!--  <div class="form-group control-group">-->
-<!--	<div class="col-md-offset-4 col-md-4">-->
-<!--	  <button type="submit" class="btn btn-success" name="login" id="login">login</button>-->
-<!--	</div>-->
-<!--  </div>-->
-<!--</form>-->
-<!--</div>-->
 <div class="jumbotron vertical-center" style="height: 100%">
     <div class="container">
         <div class="row justify-content-center">
@@ -56,19 +35,3 @@
         </div>
     </div>
 </div>
-<!--<div class="row justify-content-center">-->
-<!--    <div class="col-md-5">-->
-<!--        <div class="card">-->
-<!--            <img class="card-img-top" src="..." alt="Card image cap">-->
-<!--            <div class="card-body">-->
-<!--                <h5 class="card-title">Card title</h5>-->
-<!--                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to-->
-<!--                    additional content. This content is a little bit longer.</p>-->
-<!--                <p class="card-text">-->
-<!--                    <small class="text-muted">Last updated 3 mins ago</small>-->
-<!--                </p>-->
-<!--            </div>-->
-<!--        </div>-->
-<!--    </div>-->
-<!--</div>-->
-
